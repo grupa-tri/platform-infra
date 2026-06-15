34fb6e70d073cfa1782b9e104fd2100e0db7a56f
Marvin Koch <marvin.info@gmx.at>
2026-06-15 18:40:22 +0200

    feat(network): add infra subnet
    Refs #9

--- network.tf
+++ network.tf
@@ -4,3 +4,10 @@ resource "google_compute_network" "infra" {
 
   depends_on = [google_project_service.required_apis]
 }
+
+resource "google_compute_subnetwork" "infra" {
+  name          = "${var.environment}-infra-subnet"
+  region        = var.region
+  network       = google_compute_network.infra.id
+  ip_cidr_range = var.subnet_cidr
+}
