5cc6b71c9711685dcb7843cc65835fbb9e6b652a
Marvin Koch <marvin.info@gmx.at>
2026-06-15 18:44:15 +0200

    feat(network): add infra Cloud NAT
    Refs #9

--- network.tf
+++ network.tf
@@ -27,3 +27,11 @@ resource "google_compute_router" "infra" {
   region  = var.region
   network = google_compute_network.infra.id
 }
+
+resource "google_compute_router_nat" "infra" {
+  name                               = "${var.environment}-infra-nat"
+  router                             = google_compute_router.infra.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
