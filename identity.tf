058dd12d2d8e4fd72ea862662e0dc26b8ee34c0a
Dejan <dejan.diljkan@outlook.com>
2026-06-18 09:32:59 +0200

    feat(identity): add cert-manager dns iam resouces
    Refs #17
    Refs #18

--- identity.tf
+++ identity.tf
@@ -18,3 +18,20 @@ resource "google_service_account_iam_member" "external_dns_wi" {
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${local.wi_pool}[platform-system/external-dns]"
 }
+
+resource "google_service_account" "cert_manager" {
+  account_id   = "${var.environment}-cert-manager"
+  display_name = "cert-manager DNS-01"
+}
+
+resource "google_project_iam_member" "cert_manager_dns_admin" {
+  project = var.project_id
+  role    = "roles/dns.admin"
+  member  = "serviceAccount:${google_service_account.cert_manager.email}"
+}
+
+resource "google_service_account_iam_member" "cert_manager_wi" {
+  service_account_id = google_service_account.cert_manager.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${local.wi_pool}[cert-manager/cert-manager]"
+}
