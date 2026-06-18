8d0e6ae1b5dfac3688e797535f772305250c873c
Dejan <dejan.diljkan@outlook.com>
2026-06-18 09:35:04 +0200

    feat(identity): add external-secrets manager iam resouces
    Refs #17
    Refs #18

--- identity.tf
+++ identity.tf
@@ -35,3 +35,20 @@ resource "google_service_account_iam_member" "cert_manager_wi" {
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${local.wi_pool}[cert-manager/cert-manager]"
 }
+
+resource "google_service_account" "external_secrets" {
+  account_id   = "${var.environment}-external-secrets"
+  display_name = "External Secrets Operator"
+}
+
+resource "google_project_iam_member" "external_secrets_accessor" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.external_secrets.email}"
+}
+
+resource "google_service_account_iam_member" "external_secrets_wi" {
+  service_account_id = google_service_account.external_secrets.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${local.wi_pool}[external-secrets/external-secrets]"
+}
