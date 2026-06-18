35a614f17e14323e1ceee1698c36664f8be13270
nikolardlvc <nikola7@live.at>
2026-06-18 17:41:29 +0200

    feat(gke): add cluster configuration
    Refs #12

--- gke.tf
+++ gke.tf
@@ -8,3 +8,50 @@ resource "google_project_iam_member" "gke_nodes_default" {
   role    = "roles/container.defaultNodeServiceAccount"
   member  = "serviceAccount:${google_service_account.gke_nodes.email}"
 }
+
+resource "google_container_cluster" "cluster" {
+  name     = "${var.environment}-${var.cluster_name}"
+  location = var.zone
+
+  network    = google_compute_network.infra.id
+  subnetwork = google_compute_subnetwork.infra.id
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  deletion_protection      = false
+
+  networking_mode = "VPC_NATIVE"
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pods"
+    services_secondary_range_name = "services"
+  }
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+
+  addons_config {
+    http_load_balancing {
+      disabled = false
+    }
+
+    config_connector_config {
+      enabled = true
+    }
+  }
+
+  logging_config {
+    enable_components = ["SYSTEM_COMPONENTS", "WORKLOADS"]
+  }
+
+  monitoring_config {
+    enable_components = ["SYSTEM_COMPONENTS"]
+  }
+
+  depends_on = [google_project_service.required_apis]
+}
