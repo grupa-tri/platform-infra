e95bcb40ed461a7d027c62e62eea101593315e85
nikolardlvc <nikola7@live.at>
2026-06-18 12:51:26 +0200

    feat(gke): grant service account default role
    Refs #11

--- gke.tf
+++ gke.tf
@@ -2,3 +2,9 @@ resource "google_service_account" "gke_nodes" {
   account_id   = "${var.environment}-gke-nodes"
   display_name = "GKE node service account"
 }
+
+resource "google_project_iam_member" "gke_nodes_default" {
+  project = var.project_id
+  role    = "roles/container.defaultNodeServiceAccount"
+  member  = "serviceAccount:${google_service_account.gke_nodes.email}"
+}
