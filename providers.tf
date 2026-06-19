c4bec146c661bad06a4cd032e4608e807c6fd58b
maksim-inic <maksim.inic@outlook.com>
2026-06-19 02:30:24 +0200

    feat(flux): configure flux provider for the gke cluster
    Refs #16

--- providers.tf
+++ providers.tf
@@ -3,3 +3,25 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
+
+data "google_client_config" "default" {}
+
+provider "flux" {
+  kubernetes = {
+    host  = "https://${google_container_cluster.cluster.endpoint}"
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.cluster.master_auth[0].cluster_ca_certificate
+    )
+  }
+
+  git = {
+    url    = var.git_url
+    branch = var.git_branch
+
+    http = {
+      username = var.git_user
+      password = var.git_token
+    }
+  }
+}
